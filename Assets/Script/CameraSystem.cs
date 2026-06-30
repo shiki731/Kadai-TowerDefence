718d2db10f3375f4b7ab05980c602598eb6736fe
shiki <ktc25A31A0022@edu.kyoto-tech.ac.jp>
2026-06-30 16:11:08 +0900

    aaa

--- Assets/Script/CameraSystem.cs
+++ Assets/Script/CameraSystem.cs
@@ -1,40 +1,19 @@
 using UnityEngine;
 
-public class AreaSystem : MonoBehaviour
+public class CameraSystem : MonoBehaviour
 {
-    //enpty objectにつける
-    //エリア番号に応じたカメラの移動範囲の制御ができるスクリプト
+    // プレイヤーにつけるスクリプト
+    // キャラの位置に応じてカメラの位置をマップに応じて変える(AreaSystemのareaNum参照)
 
-    public static int areaNum = 1;
+    private float CameraPosX;
+    private float CameraPosY;
 
-    public static float camRight = 0.0f;
-    public static float camLeft = 0.0f;
-    public static float camTop = 0.0f;
-    public static float camDown = 0.0f;
-
-    public static Vector2 AreaCenter;
-
-    [SerializeField, Header("AreaSize(sprite)でカメラの動く範囲を管理")]
-    public Transform[] AreaSize;
-
-
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        camRight = Mathf.Clamp(AreaSize[areaNum - 1].localScale.x / 2 - 9.6f, 0, 1000); //AreaSizeの右端の座標 = AreaSizeの横サイズの半分-カメラのサイズ(推定),0以下にならないようにする
-        camLeft = Mathf.Clamp(AreaSize[areaNum - 1].localScale.x / 2 * -1 + 9.6f, -1000, 0);
-        camTop = Mathf.Clamp(AreaSize[areaNum - 1].localScale.y / 2 - 5.4f, 0, 1000);
-        camDown = Mathf.Clamp(AreaSize[areaNum - 1].localScale.y / 2 * -1 + 5.4f, -1000, 0);
+        CameraPosX = Mathf.Clamp(transform.position.x, CameraMoveRange.camLeft + CameraMoveRange.AreaCenter.x, CameraMoveRange.camRight + CameraMoveRange.AreaCenter.x);
+        CameraPosY = Mathf.Clamp(transform.position.y, CameraMoveRange.camDown + CameraMoveRange.AreaCenter.y, CameraMoveRange.camTop + CameraMoveRange.AreaCenter.y);
 
-        AreaCenter = new Vector2(AreaSize[areaNum - 1].position.x, AreaSize[areaNum - 1].position.y);
+        Camera.main.transform.position = new Vector3(CameraPosX, CameraPosY, -10);
+
     }
 }
--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    private float speed = 2.0f;
+    private float speed = 1.0f;
     public GameSystem system;
+    public Transform playerPos;
     public int damage = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +16,18 @@ public class EnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        float angle = GetAngle(transform.position,playerPos.position);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    float GetAngle(Vector2 fromP,Vector2 toP)
+    {
+        float dx = toP.x - fromP.x;
+        float dy = toP.y - fromP.y;
+        float rad = Mathf.Atan2(dy, dx);
+        return rad * Mathf.Rad2Deg;
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
